9723917c751b5503dec0e658550019fff5c1f5fb
Paulo Junior Schüssler <paulojuniorpjs@hotmail.com>
2023-06-24 13:45:18 -0300

    feat: Contracts API's.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { sequelize } = require('./model')
+const { getProfile } = require('./middleware/getProfile')
+const { Op } = require("sequelize")
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
@@ -11,11 +12,38 @@ app.set('models', sequelize.models)
  * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
+app.get('/contracts/:id', getProfile, async (req, res) => {
+
+    const { Contract } = req.app.get('models')
+    const { id } = req.params
+    const profileId = Number(req.get('profile_id'))
+
+    const contract = await Contract.findOne({
+        where: {
+            id: id,
+            [Op.or]: [{ ClientId: profileId }, { ContractorId: profileId }],
+        }
+    })
+
+    if (!contract) return res.status(404).end()
     res.json(contract)
 })
+
+app.get('/contracts/', getProfile, async (req, res) => {
+
+    const { Contract } = req.app.get('models')
+    const { id } = req.params
+    const profileId = Number(req.get('profile_id'))
+
+    const contract = await Contract.findAll({
+        where: {
+            status: { [Op.not]: 'terminated' },
+            [Op.or]: [{ ClientId: profileId }, { ContractorId: profileId }],
+        }
+    })
+
+    if (!contract) return res.status(404).end()
+    res.json(contract)
+})
+
 module.exports = app;
